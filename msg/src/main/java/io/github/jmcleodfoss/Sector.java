fa052f51d3d3285660c5fedc3a197874d6df7ae2
James McLeod <jmcleodfoss@gmail.com>
2020-02-03 11:18:50 -0500

    Add main function for testing

--- msg/src/main/java/io/github/jmcleodfoss/Sector.java
+++ msg/src/main/java/io/github/jmcleodfoss/Sector.java
@@ -36,6 +36,33 @@ class Sector {
 		mbb.get(data);
 		return data;
 	}
+
+	public static void main(final String[] args)
+	{
+		if (args.length == 0) {
+			System.out.println("use:\n\tjava io.github.jmcleodfoss.mst.Sector msg-file [log-level]");
+			System.exit(1);
+		}
+		try {
+			java.util.logging.Level logLevel = args.length >= 2 ? Debug.getLogLevel(args[1]) : java.util.logging.Level.OFF;
+			java.util.logging.Logger logger = java.util.logging.Logger.getLogger("io.github.jmcleodfoss.msg");
+			logger.setLevel(logLevel);
+
+			java.io.File file = new java.io.File(args[0]);
+			java.io.FileInputStream stream = new java.io.FileInputStream(file);
+			java.nio.channels.FileChannel fc = stream.getChannel();
+			java.nio.MappedByteBuffer mbb = fc.map(java.nio.channels.FileChannel.MapMode.READ_ONLY, 0, fc.size());
+			mbb.order(java.nio.ByteOrder.LITTLE_ENDIAN);
+
+			Header header = new Header(mbb);
+			Sector s = new Sector(header);
+			byte[] sector = s.read(mbb, 0);
+			for (int i = 0; i < header.sectorSize; i+=4)
+				System.out.printf("%02d: 0x%02x 0x%02x 0x%02x 0x%02x\n", i/4, sector[i], sector[i+1], sector[i+2], sector[i+3]);
+		} catch (final Exception e) {
+			e.printStackTrace(System.out);
+		}
+	}
 }
 
 
