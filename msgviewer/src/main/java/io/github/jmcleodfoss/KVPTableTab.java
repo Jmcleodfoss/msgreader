fd28aa8de95f0f2a060471f86a0d46f8c2b494f1
James McLeod <jmcleodfoss@gmail.com>
2020-02-24 17:38:35 -0500

    Optionally support showing data in a wide cell

--- msgviewer/src/main/java/io/github/jmcleodfoss/KVPTableTab.java
+++ msgviewer/src/main/java/io/github/jmcleodfoss/KVPTableTab.java
@@ -9,11 +9,17 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 class KVPTableTab<K, V> extends Tab
 {
@@ -95,6 +101,11 @@ class KVPTableTab<K, V> extends Tab
 
 	KVPTableTab(String tabName, String keyColumnName, String valueColumnName)
 	{
+		this(tabName, keyColumnName, valueColumnName, false);
+	}
+
+	KVPTableTab(String tabName, String keyColumnName, String valueColumnName, boolean fWideData)
+	{
 		super(tabName);
 
 		table = new TableView<TableData>();
@@ -104,10 +115,60 @@ class KVPTableTab<K, V> extends Tab
 
 		TableColumn<TableData, String> valueColumn = new TableColumn<TableData, String>(valueColumnName);
 		valueColumn.setCellValueFactory(new PropertyValueFactory("value"));
-
+		if (fWideData)
+			valueColumn.setCellFactory(new wideCellFactoryCallback());
 		table.getColumns().setAll(keyColumn, valueColumn);
 
 		setContent(table);
+	}
+
+	/* This was out of control as an inline class. */
+	private class wideCellFactoryCallback implements Callback<TableColumn<TableData, String>, TableCell<TableData, String>>
+	{
+		private double getTableUsedWidth()
+		{
+			double col1Width = table.getColumns().get(0).getWidth();
+
+			/* Find the width of the vertical scrollbar (include it even if it won't be shown) */
+			Iterator<Node> iter = table.getChildrenUnmodifiable().iterator();
+			while (iter.hasNext()){
+				Node n = iter.next();
+				if (!(n instanceof Parent))
+					continue;
+				Iterator<Node> iter2 = ((Parent)n).getChildrenUnmodifiable().iterator();
+				while (iter2.hasNext()){
+					Node n2 = iter2.next();
+					if (!(n2 instanceof ScrollBar))
+						continue;
+					ScrollBar sb = (ScrollBar)n2;
+					if (sb.getOrientation() == Orientation.VERTICAL){
+						// It's a little more aesthetically pleasing to not
+						// go right up to the scrollbar.
+						return col1Width + 2*sb.getWidth();
+					}
+				}
+			}
+			return col1Width;
+		}
+
+		@Override
+		public TableCell<TableData, String> call(TableColumn<TableData, String> param){
+			final TableCell<TableData, String> cell = new TableCell<TableData, String>() {
+				private Text text;
+				@Override
+				public void updateItem(String item, boolean empty){
+					super.updateItem(item, empty);
+					if (!isEmpty()){
+						text = new Text(item);
+						double newWidth = table.getWidth() - getTableUsedWidth();
+						text.setWrappingWidth(newWidth);
+						table.getColumns().get(1).setPrefWidth(newWidth);
+						setGraphic(text);
+					}
+				}
+			};
+			return cell;
+		}
 	}
 
 	void update(KVPArray<K, V> data, LocalizedText localizer)
