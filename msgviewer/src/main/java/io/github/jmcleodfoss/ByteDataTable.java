e1e4e7a0fd2150d178727c07ed2fc126d638f2bc
James McLeod <jmcleodfoss@gmail.com>
2020-02-28 22:23:50 -0500

    Classes for displaying tables of byte data

---
+++ msgviewer/src/main/java/io/github/jmcleodfoss/ByteDataTable.java
@@ -0,0 +1,130 @@
+package io.github.jmcleodfoss.msgviewer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+class ByteDataTable extends TableView<ByteDataTable.Row>
+{
+	static private final Text WIDEST_BYTE_STRING = new Text(" 88");
+
+	/** Convenience class for displaying byte arrays in a TableView */
+	static public class Row {
+		private ListProperty<Byte> columns;
+
+		public ListProperty<Byte> getColumnsProperty()
+		{
+			if (columns == null) columns = new SimpleListProperty(this, "columns");
+			return columns;
+		}
+		public ObservableList<Byte> getColumns()
+		{
+			return getColumnsProperty().get();
+		}
+		public void setColumns(ObservableList<Byte> value)
+		{
+			getColumnsProperty().set(value);
+		}
+
+		Row(byte[] data)
+		{
+			ArrayList<Byte> al = new ArrayList<Byte>(data.length);
+			for(byte b: data)
+				al.add(b);
+			ObservableList<Byte> ol = FXCollections.observableArrayList(al);
+			columns = new SimpleListProperty<Byte>(ol);
+		}
+	}
+
+	public class HexTableCell extends ListPropertyEntryValueFactory<Byte, Row, String>
+	{
+		HexTableCell(String property, int index)
+		{
+			super(property, index);
+		}
+
+		@Override
+		public ObservableValue<String> call(TableColumn.CellDataFeatures<Row, String> param)
+		{
+			return new ReadOnlyObjectWrapper(String.format("%02x", getValue(param)));
+		}
+	}
+
+	public class ASCIITableCell extends ListPropertyEntryValueFactory<Byte, Row, String>
+	{
+		ASCIITableCell(String property, int index)
+		{
+			super(property, index);
+		}
+
+		@Override
+		public ObservableValue<String> call(TableColumn.CellDataFeatures<Row, String> param)
+		{
+			ReadOnlyObjectWrapper o = (ReadOnlyObjectWrapper)super.call(param);
+			ObservableList<Byte> ol = (ObservableList)o.get();
+			int index = getIndex();
+			byte highByte = ol.get(index);
+			byte lowByte  = ol.get(index+1);
+			char codepoint = (char)(0xffff & (highByte << 8 | lowByte));
+			if (!java.lang.Character.isDefined(codepoint))
+				codepoint = 0;
+			return new ReadOnlyObjectWrapper(String.format("%c", codepoint));
+		}
+	}
+
+	private int nColumns;
+
+	ByteDataTable(String[] headings, boolean fShowUnicode)
+	{
+		super();
+		assert headings.length %2 == 0;
+		double cellWidth = WIDEST_BYTE_STRING.getBoundsInLocal().getWidth();
+
+		int dataLength = headings.length;
+		nColumns = dataLength;
+		if (fShowUnicode)
+			++nColumns;
+
+		ArrayList<TableColumn<Row, String>> columns = new ArrayList<TableColumn<Row, String>>();
+		for (int i = 0; i < dataLength; ++i) {
+			TableColumn<Row, String> col = new TableColumn<Row, String>(headings[i]);
+		  	col.setCellValueFactory(new HexTableCell("columns", i));
+			col.setPrefWidth(cellWidth);
+			columns.add(col);
+  		}
+		if (fShowUnicode){
+			TableColumn<Row, String> unicodeData = new TableColumn<Row, String>("Unicode");
+			columns.add(unicodeData);
+			for (int i = 0; i < dataLength/2; ++i) {
+				TableColumn<Row, String> col = new TableColumn<Row, String>();
+			  	col.setCellValueFactory(new ASCIITableCell("columns", i));
+				col.setPrefWidth(cellWidth/2);
+				unicodeData.getColumns().add(col);
+			}
+		}
+
+		getColumns().setAll(columns);
+	}
+
+	void update(byte[] data)
+	{
+		ObservableList<Row> a = FXCollections.observableArrayList();
+
+		int srcIndex = 0;
+		while (srcIndex < data.length){
+			byte[] rowBytes = Arrays.copyOfRange(data, srcIndex, srcIndex + nColumns);
+			a.add(new Row(rowBytes));
+			srcIndex += nColumns;
+		}
+		setItems(a);
+	}
+}
---
+++ msgviewer/src/main/java/io/github/jmcleodfoss/ListPropertyEntryValueFactory.java
@@ -0,0 +1,111 @@
+package io.github.jmcleodfoss.msgviewer;
+
+import java.util.List;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableColumn;
+
+/* Cell factory for use when property lists provide cell data for a column.
+ * The property list can be of any kind; the user-defined call function converts
+ * it to the appropriate type for display (typically a String).
+ *
+ * Use
+ *
+ * 	// Class variable
+ * 	class Row
+ * 	{
+ * 		private ListProperty<Byte> columns;
+ * 		public setColumns(ListProperty<Byte> columns)
+ * 		{
+ * 			columnsProperty().set(columns);
+ * 		}
+ * 		public getColumns()
+ * 		{
+ * 			return columnsProperty().get();
+ * 		}
+ * 		public columnsProperty()
+ * 		{
+ * 			if (columns == null) columns = new SimpleListProperty<Byte>(this, "columns");
+ * 			return columns;
+ * 		}
+ *
+ *		Row(byte[] data)
+ *		{
+ *			ArrayList<Byte> al = new ArrayList<Byte>(data.length);
+ *			for (byte b: data)
+ *				al.add(b);
+ *			ObservableList<Byte> ol = FXCollections.observableArrayList(al);
+ *			columns = new SimpleListProperty<Byte>(ol);
+ *		}
+ * 	}
+ *
+ * 	Row[] columns;
+ *
+ * 	public static void main(String[] args)
+ * 	{
+ * 		String[] headers = {"Col 1", "Col B", "Col iii", "Col four" };
+ *
+ *		ObservableList<Row> rows = FXCollections.observableArrayList();
+ *
+ * 		int nColumns = headings.length;
+ *		TableColumn<Row, String> columns = new TableColumn<Row, String>[nColumns];
+ * 	
+ *	 	for (int i = 0; i < nColumns; ++i){
+ * 			col[i] = new TableColumn<Row, String>(headers[i]);
+ * 			col[i].setCellValueFactory(
+ * 				new ListPropertyEntryValueFactory<Byte, Rows, String>("columnsData", i){
+ *					@Override
+ *					public ObservableValue<String> call(TableColumn.CellDataFeatures<Rows,String> param)
+ *					{
+ *						byte b = getValue(param);
+ *						return new ReadOnlyObjectWrapper(String.format("%02x", getValue(param)));
+ *					}
+ * 				}
+ * 			);
+ * 		}
+ *
+ * 		TreeView tree = new TreeView(rows);
+ * 	}
+ *
+ * Notes
+ *
+ * 	R: data type of the list associated with this factory.
+ * 	S: row data type
+ * 	T: cell type
+ */
+class ListPropertyEntryValueFactory<R, S, T> extends PropertyValueFactory<S, T>
+{
+	/** The index into the list backing this property */
+	private int index;
+
+	/** Create a ListPropertyEntryValueFactory
+	*	@param	property	The list (as a property) to use when creating the cell content
+	*	@param	index		The index into the list
+	*/
+	ListPropertyEntryValueFactory(String property, int index)
+	{
+		super(property);
+		this.index = index;
+	}
+
+	/** Get the value from the underlying list so it can be manipulated for
+	*   display.
+	*	@param	param	The row object and ancillary information
+	*	@return	The value to base the display on
+	*/
+	R getValue(TableColumn.CellDataFeatures<S,T> param)
+	{
+		ReadOnlyObjectWrapper o = (ReadOnlyObjectWrapper)super.call(param);
+		return (R)((ObservableList)o.get()).get(index);
+	}
+
+	/** Get the index into the property list for this object.
+	*	@return	The index into the property list saved in the constructor
+	*/
+	public final int getIndex()
+	{
+		return index;
+	}
+}
