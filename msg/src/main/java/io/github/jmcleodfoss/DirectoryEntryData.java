d90f5fcc4830745b887a534c685f7c8963e6a63f
James McLeod <jmcleodfoss@gmail.com>
2020-03-02 16:48:33 -0500

    New class for passing directory entry info to client applications

---
+++ msg/src/main/java/io/github/jmcleodfoss/DirectoryEntryData.java
@@ -0,0 +1,46 @@
+package io.github.jmcleodfoss.msg;
+
+/** This class is used to publish directory entry info to a client applicaiton */
+public class DirectoryEntryData {
+	/** The directory entry index */
+	public final int entry;
+
+	/** The directory entry name */
+	public final String name;
+
+	/** The entry's children */
+	public final java.util.ArrayList<Integer> children;
+
+	/** The entry's size */
+	public final int size;
+
+	/** The entry's starting sector */
+	public final int startingSector;
+
+	/** All directory information */
+	public final KVPArray<String, String> kvps;
+
+	/** Create the external data object for the given directory entry
+	*	@param	directory	The Directory object the entry is from
+	*	@param	entry	The index of the directory entry to retreive
+	*/
+	DirectoryEntryData(Directory directory, int entry)
+	{
+		this.entry = entry;
+		final DirectoryEntry de = directory.entries.get(entry);
+		name = de.directoryEntryName;
+		children = directory.getChildren(entry);
+		size = (int)de.streamSize;
+		startingSector = de.startingSectorLocation;
+
+		kvps = de.data();
+	}
+
+	/** Create a string representing this directory entry
+	*	@return	The name of the entry
+	*/
+	public String toString()
+	{
+		return name;
+	}
+}
