23b22ee128f80c6d2958849bb8be84d87cb02689
James McLeod <jmcleodfoss@gmail.com>
2020-03-06 10:12:12 -0500

    Standardize Javadoc formatting

--- msg/src/main/java/io/github/jmcleodfoss/DataContainer.java
+++ msg/src/main/java/io/github/jmcleodfoss/DataContainer.java
@@ -1,6 +1,6 @@
 package io.github.jmcleodfoss.msg;
 
-/**	The DataContainer class may be used to manage a set up DataDefinition objects, saving all values read in a map member.
+/** The DataContainer class may be used to manage a set up DataDefinition objects, saving all values read in a map member.
 *	It provides utility methods for retrieving various useful types.
 *	Note that it is based on java.util.IdentityHashMap, so it is necessary to "get" a value using the identical argument used to
 *	"put" it.
@@ -9,14 +9,13 @@ package io.github.jmcleodfoss.msg;
 */
 public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 
-	/**	The IncompleteInitializationException indicates that a required component is not available at the time it is required. */
+	/** The IncompleteInitializationException indicates that a required component is not available at the time it is required. */
 	static class IncompleteInitializationException extends RuntimeException {
 
-		/**	The serialVersionUID is required because the base class is serializable. */
+		/** The serialVersionUID is required because the base class is serializable. */
 		private static final long serialVersionUID = 1L;
 
-		/**	Create an IncompleteInitializationException for the given field.
-		*
+		/** Create an IncompleteInitializationException for the given field.
 		*	@param	component	The name of the component requested but not found.
 		*/
 		public IncompleteInitializationException(final String component)
@@ -25,17 +24,16 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 		}
 	}
 
-	/**	The serialVersionUID is required because the base class is serializable. */
+	/** The serialVersionUID is required because the base class is serializable. */
 	private static final long serialVersionUID = 1L;
 
-	/**	Constructor with default maximum size .*/
+	/** Constructor with default maximum size .*/
 	public DataContainer()
 	{
 		super();
 	}
 
-	/**	Constructor with a known expected maximum number of entries.
-	*
+	/** Constructor with a known expected maximum number of entries.
 	*	@param	expectedMaxSize	The number of entries which will be put in the underlying collection.
 	*/
 	DataContainer(final int expectedMaxSize)
@@ -43,10 +41,8 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 		super(expectedMaxSize);
 	}
 
-	/**	Return an unsigned 8-bit value.
-	*
+	/** Return an unsigned 8-bit value.
 	*	@param	name	The name of the value to return.
-	*
 	*	@return	An integer containing the unsigned 8-bit value.
 	*/
 	int getUInt8(String name)
@@ -54,11 +50,9 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 		return 0xff & (Byte)get(name);
 	}
 
-	/**	Read in all descriptions from the given data stream.
-	*
+	/** Read in all descriptions from the given data stream.
 	*	@param	byteBuffer	The input stream from which to read the data.
 	*	@param	description	The list of descriptions of data to be read.
-	*
 	*	@throws	java.io.IOException	An I/O problem was encountered while reading in the requested data.
 	*/
 	public void read(java.nio.ByteBuffer byteBuffer, final DataDefinition... description)
@@ -69,11 +63,9 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 			DataDefinition.read(d, byteBuffer, this);
 	}
 
-	/**	Read in all descriptions from the given data stream.
-	*
+	/** Read in all descriptions from the given data stream.
 	*	@param	byteBuffer	The input stream from which to read the data.
 	*	@param	description	The list of descriptions of data to be read.
-	*
 	*	@throws	java.io.IOException	An I/O problem was encountered while reading in the requested data.
 	*/
 	void read(java.nio.ByteBuffer byteBuffer, final DataDefinition[]... description)
@@ -84,8 +76,7 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 			read(byteBuffer, d);
 	}
 
-	/**	Confirm the given field was read in; throw an exception if it wasn't.
-	*
+	/** Confirm the given field was read in; throw an exception if it wasn't.
 	*	@param	nm_field	The name of the field to check.
 	*/
 	protected void validateFieldExists(final String nm_field)
@@ -94,9 +85,8 @@ public class DataContainer extends java.util.IdentityHashMap<String, Object> {
 			throw new IncompleteInitializationException(nm_field);
 	}
 
-	/**	Format and return a human-readable representation of this DataContainer with the property names and hex byte strings for
-	*	each.
-	*
+	/** Format and return a human-readable representation of this
+	*   DataContainer with the property names and hex byte strings for each.
 	*	@return	A String showing the contents of this DataContainer object.
 	*/
 	@Override
