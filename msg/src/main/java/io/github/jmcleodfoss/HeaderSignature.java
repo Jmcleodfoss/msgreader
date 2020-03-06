50a6a0352a790ea22bd23a3dfc7c9864224341d9
James McLeod <jmcleodfoss@gmail.com>
2020-03-06 06:49:48 -0500

    Use shorter invocation of MakeLongLE

--- msg/src/main/java/io/github/jmcleodfoss/HeaderSignature.java
+++ msg/src/main/java/io/github/jmcleodfoss/HeaderSignature.java
@@ -7,7 +7,7 @@ class HeaderSignature {
 	static final byte[] SIGNATURE_BYTES = {(byte)0xd0, (byte)0xcf, (byte)0x11, (byte)0xe0, (byte)0xa1, (byte)0xb1, (byte)0x1a, (byte)0xe1};
 
 	/** The header signature as an 8-byte (long) value. */
-	static final long SIGNATURE = io.github.jmcleodfoss.msg.ByteUtil.makeLongLE(SIGNATURE_BYTES, 8);
+	static final long SIGNATURE = io.github.jmcleodfoss.msg.ByteUtil.makeLongLE(SIGNATURE_BYTES);
 
 	/** Validate that the passed signature is a valid header signature.
 	*	@param	signature	The signature to be checked
