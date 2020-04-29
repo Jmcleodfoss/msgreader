e62371f488e5b0473409e6879bb35094bd880ec3
James McLeod <jmcleodfoss@gmail.com>
2020-04-29 05:08:29 +0100

    Make signature bytes and value private

--- msg/src/main/java/io/github/jmcleodfoss/msg/HeaderSignature.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/HeaderSignature.java
@@ -8,10 +8,10 @@ class HeaderSignature {
 	/** The bytes which form the signature, in the order in which they are documented.
 	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
 	*/
-	static final byte[] SIGNATURE_BYTES = {(byte)0xd0, (byte)0xcf, (byte)0x11, (byte)0xe0, (byte)0xa1, (byte)0xb1, (byte)0x1a, (byte)0xe1};
+	private static final byte[] SIGNATURE_BYTES = {(byte)0xd0, (byte)0xcf, (byte)0x11, (byte)0xe0, (byte)0xa1, (byte)0xb1, (byte)0x1a, (byte)0xe1};
 
 	/** The header signature as an 8-byte (long) value. */
-	static final long SIGNATURE = io.github.jmcleodfoss.msg.ByteUtil.makeLongLE(SIGNATURE_BYTES);
+	private static final long SIGNATURE = io.github.jmcleodfoss.msg.ByteUtil.makeLongLE(SIGNATURE_BYTES);
 
 	/** Validate that the passed signature is a valid header signature.
 	*	@param	signature	The signature to be checked
