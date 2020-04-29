1848604f62fc4ddd5ef6c4af830a47f576621c19
James McLeod <jmcleodfoss@gmail.com>
2020-04-29 05:07:36 +0100

    Add some cross-references to MS-CFB

--- msg/src/main/java/io/github/jmcleodfoss/msg/HeaderSignature.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/HeaderSignature.java
@@ -1,9 +1,13 @@
 package io.github.jmcleodfoss.msg;
 
-/** The file signature. */
+/** The file signature.
+*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
+*/
 class HeaderSignature {
 
-	/** The bytes which form the signature, in the order in which they are documented. */
+	/** The bytes which form the signature, in the order in which they are documented.
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
+	*/
 	static final byte[] SIGNATURE_BYTES = {(byte)0xd0, (byte)0xcf, (byte)0x11, (byte)0xe0, (byte)0xa1, (byte)0xb1, (byte)0x1a, (byte)0xe1};
 
 	/** The header signature as an 8-byte (long) value. */
