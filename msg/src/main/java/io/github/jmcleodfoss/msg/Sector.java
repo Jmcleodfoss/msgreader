8ea9e0d86dc20709c8cf0e21613ef3c76058b20f
James McLeod <jmcleodfoss@gmail.com>
2020-04-29 04:10:40 +0100

    Fix, standardize, and complete Javadoc

--- msg/src/main/java/io/github/jmcleodfoss/msg/Sector.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/Sector.java
@@ -1,24 +1,38 @@
 package io.github.jmcleodfoss.msg;
 
-/** Constants and classes for dealing with sectors */
+/** Constants and classes for dealing with sectors
+*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+*/
 class Sector {
 
-	/** The maximum regular sector index */
+	/** The maximum regular sector index
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int MAXREGSEC = 0xfffffffa;
 
-	/** A reserved sector */
+	/** A reserved sector
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int RESERVED = 0xfffffffb;
 
-	/** A DIFAT (Double Indirect File Allocation Table) sector */
+	/** A DIFAT (Double Indirect File Allocation Table) sector
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int DIFSECT = 0xfffffffc;
 
-	/** A FAT (File Allocation Table) sector */
+	/** A FAT (File Allocation Table) sector
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int FATSECT = 0xfffffffd;
 
-	/** The end of a chain of sectors */
+	/** The end of a chain of sectors
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int ENDOFCHAIN = 0xfffffffe;
 
-	/** A free (unused) sector */
+	/** A free (unused) sector
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/9d33df18-7aee-4065-9121-4eabe41c29d4">MS-CFB Section 2,1: Compound Sector Numbers and Types</a>
+	*/
 	static final int FREESECT = 0xffffffff;
 
 	/** Get a description of the given sector ID
@@ -40,7 +54,9 @@ class Sector {
 	}
 
 	/** Get the sector size given the sector shift.
-	*   @param	sectorShift	The sector shift value (2 is shifted by this number to get the sector size)
+	*	@param	sectorShift	The sector shift value (2 is shifted by this number to get the sector size)
+	*	@return	The sector size corresponding to the given sectorShift.
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
 	*/
 	static int sectorSize(short sectorShift)
 	{
