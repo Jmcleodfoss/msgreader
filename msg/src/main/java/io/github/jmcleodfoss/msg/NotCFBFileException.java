ab74608475e3269f23329297e8e3b802dce62529
James McLeod <jmcleodfoss@gmail.com>
2020-04-30 14:05:55 +0100

    Add some Javadoc cross-references

--- msg/src/main/java/io/github/jmcleodfoss/msg/NotCFBFileException.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/NotCFBFileException.java
@@ -1,6 +1,9 @@
 package io.github.jmcleodfoss.msg;
 
-/** The NotCFBFileException is thrown when the first eight bytes of the file are not the CFB file signature bytes.  */
+/** The NotCFBFileException is thrown when the first eight bytes of the file are not the CFB file signature bytes.
+*	@see HeaderSignature 
+*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
+*/
 public class NotCFBFileException extends Exception {
 
 	/**	The serialVersionUID is required because the base class is serializable. */
