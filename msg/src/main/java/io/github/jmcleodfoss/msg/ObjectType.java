72a90be1deaa1b82d3eb64e0fa5deaa056ea9420
James McLeod <jmcleodfoss@gmail.com>
2020-05-01 05:11:38 +0100

    Add some more cross-references

--- msg/src/main/java/io/github/jmcleodfoss/msg/ObjectType.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/ObjectType.java
@@ -1,19 +1,27 @@
 package io.github.jmcleodfoss.msg;
 
-/** The ObjectType class represents a CFB entry object type; it can be any of { Unknown, Storage, Stream, Root Storage. }
+/** The ObjectType class represents a CFB entry object type; it can be one of { {@link #UNKNOWN}, {@link #STORAGE}, {@link #STREAN}, {@link ROOT_STORATE} }
  * @see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/60fe8611-66c3-496b-b70d-a504c94c9ace">MS-CFB 2.6.1 Compound File Directory Entry</a>
  */
 class ObjectType {
-	/** The object type for Unknown or Unallocated entries. */
+	/** The object type for Unknown or Unallocated entries.
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/60fe8611-66c3-496b-b70d-a504c94c9ace">MS-CFB 2.6.1 Compound File Directory Entry</a>
+	*/
 	private static final byte UNKNOWN = 0x00;
 
-	/** The object type for Storage Objects */
+	/** The object type for Storage Objects 
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/60fe8611-66c3-496b-b70d-a504c94c9ace">MS-CFB 2.6.1 Compound File Directory Entry</a>
+	*/
 	private static final byte STORAGE = 0x01;
 
-	/** The object type for Stream Objects */
+	/** The object type for Stream Objects
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/60fe8611-66c3-496b-b70d-a504c94c9ace">MS-CFB 2.6.1 Compound File Directory Entry</a>
+	*/
 	private static final byte STREAM = 0x02;
 
-	/** The object type for Root Storage Objects */
+	/** The object type for Root Storage Objects
+	*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/60fe8611-66c3-496b-b70d-a504c94c9ace">MS-CFB 2.6.1 Compound File Directory Entry</a>
+	*/
 	private static final byte ROOT_STORAGE = 0x05;
 
 	/** The actual object type. */
