2dfe904bce8544027cc686a49664df57fab905b5
James McLeod <jmcleodfoss@gmail.com>
2020-05-01 05:14:00 +0100

    Add Javadoc for misisng parameter for constructor

--- msg/src/main/java/io/github/jmcleodfoss/msg/ObjectType.java
+++ msg/src/main/java/io/github/jmcleodfoss/msg/ObjectType.java
@@ -27,7 +27,9 @@ class ObjectType {
 	/** The actual object type. */
 	private final byte type;
 
-	/* Construct an ObjectType from the given byte read out of a directory entry block. */
+	/* Construct an ObjectType from the given byte read out of a directory entry block
+	*	@param	type	The type as read
+	*/
 	ObjectType(byte type)
 	{
 		if (type != UNKNOWN && type != STORAGE && type != STREAM && type != ROOT_STORAGE)
