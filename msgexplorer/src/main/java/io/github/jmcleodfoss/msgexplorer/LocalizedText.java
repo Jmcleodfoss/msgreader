8519caf8b684623b13f3db3a880100634faf20ba
James McLeod <jmcleodfoss@gmail.com>
2020-05-05 16:23:19 +0100

    Add Javadoc

--- msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/LocalizedText.java
+++ msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/LocalizedText.java
@@ -3,23 +3,36 @@ package io.github.jmcleodfoss.msgexplorer;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/** Wrapper for ResourceBundle to allow easy localization */
 class LocalizedText
 {
+	/** The name of the resource bundle to load. *
 	private final String RESOURCE_SOURCE = "io.github.jmcleodfoss.msgexplorer.text";
+
+	/** The locale we are running it. */
 	private Locale locale;
+
+	/** The respource bundle with localized text. There is an English version of this showing all the property names and their English values. */
 	private ResourceBundle resources;
 
+	/** Create a localization object for the default locale */
 	LocalizedText()
 	{
 		this(Locale.getDefault());
 	}
 
+	/** Create a localization object for the given locale
+	*	@param	locale	The locale to localize for
+	*/
 	LocalizedText(java.util.Locale locale)
 	{
 		this.locale = locale;
 		resources = ResourceBundle.getBundle(RESOURCE_SOURCE, locale, this.getClass().getClassLoader());
 	}
 
+	/** Get the localized taxt for the stored locale for the given property name
+	*	@param	property	The property name to retrive the localized text for.
+	*/
 	String getText(String property)
 	{
 		if (resources.keySet().contains(property))
