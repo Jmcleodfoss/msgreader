0734198b2b9842060584111f561e98afc95af009
James McLeod <jmcleodfoss@gmail.com>
2020-05-07 02:17:54 +0100

    Add Javadoc

--- msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/Header.java
+++ msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/Header.java
@@ -2,6 +2,9 @@ package io.github.jmcleodfoss.msgexplorer;
 
 import io.github.jmcleodfoss.msg.MSG;
 
+/** Tab displaying the file header
+*	@see <a href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-cfb/05060311-bfce-4b12-874d-71fd4ce63aea">MS-CFB Section 2.2: Compound File Header</a>
+*/
 class Header extends KVPTableTab<String, String>
 {
 	/* Properties for the tab name and table column headings */
@@ -9,11 +12,18 @@ class Header extends KVPTableTab<String, String>
 	static private final String KEY_HEADING = "header.display.key-heading";
 	static private final String VALUE_HEADING = "header.display.value-heading";
 
+	/** Create the header display tab.
+	*	@param	localizer	The localizer mapping for the current locale.
+	*/
 	Header(LocalizedText localizer)
 	{
 		super(localizer.getText(TAB_TITLE), localizer.getText(KEY_HEADING), localizer.getText(VALUE_HEADING));
 	}
 
+	/** Update the header display.
+	*	@param	msg	The msg object for the file we are displaying
+	*	@param	localizer	The localizer mapping for the current locale.
+	*/
 	void update(MSG msg, LocalizedText localizer)
 	{
 		if (msg == null)
