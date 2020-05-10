ddc58c23867e6ffcbcb64510c77c4436a78927c9
James McLeod <jmcleodfoss@gmail.com>
2020-05-10 19:50:27 +0100

    Display attachment information

--- example_commandline/src/main/java/io/github/jmcleodfoss/example_commandline/CommandlineExample.java
+++ example_commandline/src/main/java/io/github/jmcleodfoss/example_commandline/CommandlineExample.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 
 public class CommandlineExample
 {
+	private static final String ATTACHMENT_INFO_FORMAT = "%-25s %-30s %-10s\n";
 	private static String getPropertyValue(MSG msg, HashMap<Integer, Property> properties, int propertyTag)
 	{
 		if (properties.keySet().contains(propertyTag))
@@ -42,6 +43,22 @@ public class CommandlineExample
 		showProperty(msg, properties, PropertyTags.PidTagSenderEmailAddress, "Email");
 		showProperty(msg, properties, PropertyTags.PidTagSubject, "Subject");
 		showProperty(msg, properties, PropertyTags.PidTagBody, "Body");
+
+		Iterator<DirectoryEntryData> attachments = msg.attachments();
+		if (attachments.hasNext()) {
+			System.out.println();
+			System.out.println("Attachment Info");
+			System.out.println("---------------");
+			System.out.printf(ATTACHMENT_INFO_FORMAT, "Long filename", "Mime type", "Size (bytes)");
+			System.out.printf(ATTACHMENT_INFO_FORMAT, "-------------------------", "-----------------------------", "---------------");
+		}
+		while (attachments.hasNext()){
+			HashMap<Integer, Property> m = msg.getPropertiesForParentAsHashMap(attachments.next());
+			String name = getPropertyValue(msg, m, PropertyTags.PidTagAttachLongFilename);
+			String mimeType = getPropertyValue(msg, m, PropertyTags.PidTagAttachMimeTag);
+			String size = getPropertyValue(msg, m, PropertyTags.PidTagAttachDataBinary);
+			System.out.printf(ATTACHMENT_INFO_FORMAT, name, mimeType, PropertyTags.PidTagAttachDataBinary);
+		}
 	}
 
 	// args[0] is the path and filename to open
