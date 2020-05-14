9a27b6ddc4e860111e492236d65e50d4f340472b
James McLeod <jmcleodfoss@gmail.com>
2020-05-14 20:28:03 +0100

    Add module-info.java for applications

---
+++ msg_example/src/main/java/io/github/jmcleodfoss/msg_example/module-info.java
@@ -0,0 +1,4 @@
+module io.github.jmcleodfoss.msg_example {
+	requires java.desktop;
+	requires io.github.jmcleodfoss.msg;
+}
---
+++ msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/module-info.java
@@ -0,0 +1,6 @@
+module io.github.jmcleodfoss.msgexplorer {
+	requires javafx.base;
+	requires javafx.controls;
+	requires io.github.jmcleodfoss.msg;
+	requires io.github.jmcleodfoss.voluminouspaginationskin;
+}
