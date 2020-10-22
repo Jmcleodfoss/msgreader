ad395044229239bec5157f86b86ebafd8ccf522d
James McLeod <jmcleodfoss@gmail.com>
2020-10-22 06:05:01 -0400

    Use double quotes around result to prevent file globbing and word splitting

--- extras/test.sh
+++ extras/test.sh
@@ -93,5 +93,5 @@ echo "Ending tests at $(date +%H:%M:%S)" >> $stats
 declare result
 result=$(grep -e "java\.lang\..*Exception" -e [a-n]Exception $results_dir/*/*.out)
 if [[ -n $result ]]; then
-	printf "Errors found\n%s" $result
+	printf "Errors found\n%s" "$result"
 fi
