68a597e17a5b9b87b7d78c628c1e8dc10579657c
James McLeod <jmcleodfoss@gmail.com>
2021-05-25 11:49:07 -0400

    Use correct linking method for javafx documenation cross-links

--- extras/get-package-list.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Run directly from the extras directory
-grep -R --include=*.java "import.*javafx" .. | sed "/^.*import \(.*\)\..*;$/s//\1/" | sort -u > javafx-package-list/package-list
