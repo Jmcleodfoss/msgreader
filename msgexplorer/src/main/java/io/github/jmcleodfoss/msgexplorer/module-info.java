5870ea1c12174bd025d9bd7b17d8f7cf0e361fc8
James McLeod <jmcleodfoss@gmail.com>
2021-05-25 15:10:35 -0400

    Add javafx.graphics as a requirement

--- msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/module-info.java
+++ msgexplorer/src/main/java/io/github/jmcleodfoss/msgexplorer/module-info.java
@@ -1,6 +1,7 @@
 module io.github.jmcleodfoss.msgexplorer {
 	requires javafx.base;
 	requires javafx.controls;
+	requires javafx.graphics;
 	requires io.github.jmcleodfoss.msg;
 	requires io.github.jmcleodfoss.voluminouspaginationskin;
 }
